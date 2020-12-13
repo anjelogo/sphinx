6e9da7ca9d830f76260a692cb9337d426cfd9a29
Deorc <deorcstudios@gmail.com>
2020-12-13 02:52:52 -0800

    Update case.js
    grabs existing embed in logs channel instead of creating a new one

--- Commands/Moderation/case.js
+++ Commands/Moderation/case.js
@@ -1,7 +1,6 @@
 const log = require("../../Internals/handlers/log"),
 	Emojis = require("../../Utils/emojis.json"),
-	{ colors } = require("../../Utils/config.json"),
-	{ search } = require("../../Internals/handlers/profileHandler");
+	{ channels } = require("../../Utils/config.json");
 
 module.exports = {
 	commands: [
@@ -23,31 +22,10 @@ module.exports = {
 
 		if (!Case) return m.edit(`${Emojis.x} Couldn't find a case with the number \`${caseNum}\`.`);
 
-		const user = await search(bot, Case.userID, msg.author, m),
-			moderator = await search(bot, Case.moderator, msg.author, m),
-			resolvedModerator = Case.resolved ? await search(bot, Case.resolved.moderator, msg.author, m) : null,
-			embed = {
-				title: `${Case.action.replace(/^\w/, c => c.toUpperCase())} ${Case.resolved ? "(Resolved) " : ""}| Case #${caseNum}`,
-				thumbnail: {
-					url: user.avatarURL
-				},
-				fields: [
-					{
-						name: "User",
-						value: `${user.mention} (${Case.userID})`,
-						inline: true
-					}, {
-						name: "Moderator",
-						value: Case.resolved ? `${resolvedModerator.mention}\n~~${moderator.mention}~~` : moderator.mention,
-						inline: true
-					}, {
-						name: "Reason",
-						value: Case.resolved ? `${Case.resolved.reason}\n~~${Case.reason}~~` : Case.reason
-					} 
-				],
-				color: Case.resolved ? colors.resolved : colors[Case.action],
-				timestamp: new Date()
-			};
+		const ms = await bot.channels.get(channels.log).getMessage(Case.messageID);
+		
+		if (!ms) return m.edit(`${Emojis.x} I couldn't load embed data!`);
+		let embed = ms.embeds[0];
 
 		m.edit({ content: "", embed });
 	}
